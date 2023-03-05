3dba13dc6f5e9ed655da2e632740d504814525b1
Olena Rolska <olenarolska@Olenas-MacBook-Pro.local>
2023-03-05 22:59:21 +0200

    test file edit an app in email signature template is prepared

---
+++ cypress/e2e/editApp.cy.js
@@ -0,0 +1,50 @@
+import "../support/commands";
+
+const beetLinkedin = "https://www.linkedin.com/company/beetroot-se/";
+const beetYoutube = "https://www.youtube.com/@beetroot5192";
+const beetInstagram = "https://www.instagram.com/beetroot.se/";
+const beetTwitter = "https://twitter.com/LoveBeetroot";
+const beetFacebook = "https://www.facebook.com/beetroot.se";
+const beetTiktok = "https://www.tiktok.com/@simplybotanical?lang=en";
+
+const wiseLinkedin = "https://www.linkedin.com/company/wisestamp/";
+const wiseYoutube = "https://www.youtube.com/@wisestamp";
+const wiseInstagram = "https://www.youtube.com/@wisestamp";
+const wiseTwitter = "https://twitter.com/WiseStamp";
+const wiseFacebook = "https://www.facebook.com/WiseStamp";
+const wiseTiktok = "https://www.tiktok.com/@wisestamp?lang=en";
+
+describe("Edit an app", () => {
+  before(() => {
+    cy.visit("https://webapp.wisestamp.com/editor");
+  });
+  it("Add an app", () => {
+    cy.addAnApp(
+      beetLinkedin,
+      beetYoutube,
+      beetInstagram,
+      beetTwitter,
+      beetFacebook,
+      beetTiktok
+    );
+  });
+  it("Check application", () => {
+    cy.checkAnApp();
+  });
+  it("Edit application", () => {
+    cy.get("[aid='page.editor.apps.social_buttons.edit']").click({
+      force: true,
+    });
+    cy.editAnApp(
+      wiseLinkedin,
+      wiseYoutube,
+      wiseInstagram,
+      wiseTwitter,
+      wiseFacebook,
+      wiseTiktok
+    );
+  });
+  it("Check application after edit", () => {
+    cy.checkAnApp();
+  });
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,111 @@
+Cypress.Commands.add(
+  "addAnApp",
+  (linkedin, youtube, instagram, twitter, facebook, tiktok) => {
+    cy.get('[aid="page.editor.signature.tabs.addons"]').click({ force: true });
+    cy.get('[aid="page.editor.apps.social_buttons"]').click();
+    cy.get("input[aid='signature_editor.input.0:appdata:target']").type(
+      linkedin,
+      { force: true }
+    );
+    cy.intercept({
+      method: "POST",
+      path: "/api/signatures/render",
+    }).as("signatureIsRendered");
+    cy.get("input[aid='signature_editor.input.1:appdata:target']").type(
+      youtube,
+      {
+        force: true,
+      }
+    );
+    cy.wait("@signatureIsRendered", { timeout: 10000 });
+    cy.get("input[aid='signature_editor.input.2:appdata:target']").type(
+      instagram,
+      { force: true }
+    );
+    cy.wait("@signatureIsRendered", { timeout: 10000 });
+    cy.get("input[aid='signature_editor.input.3:appdata:target']").type(
+      twitter,
+      {
+        force: true,
+      }
+    );
+    cy.wait("@signatureIsRendered", { timeout: 10000 });
+    cy.get("input[aid='signature_editor.input.4:appdata:target']").type(
+      facebook,
+      {
+        force: true,
+      }
+    );
+    cy.wait("@signatureIsRendered", { timeout: 10000 });
+    cy.get(".listControl").click();
+    cy.get("input[aid='signature_editor.input.8:appdata:target']").type(
+      tiktok,
+      {
+        force: true,
+      }
+    );
+    cy.wait("@signatureIsRendered", { timeout: 10000 });
+    cy.get("img[alt='App Social Buttons Image']", { timeout: 5000 });
+
+    cy.get(".btn__confirm").find("button").should("not.be.disabled");
+    cy.get(".btn__confirm").find("button").click({ force: true });
+    cy.wait("@signatureIsRendered", { timeout: 10000 });
+  }
+);
+
+Cypress.Commands.add("checkAnApp", () => {
+  cy.get(".addons__list").should(($addon) => {
+    expect($addon).to.contain("Social Buttons");
+  });
+
+  cy.get("td > a", { timeout: 10000 }).should(($elem) => {
+    expect($elem).to.have.length(6);
+  });
+});
+
+Cypress.Commands.add(
+  "editAnApp",
+  (linkedin, youtube, instagram, twitter, facebook, tiktok) => {
+    cy.get("input[aid='signature_editor.input.0:appdata:target']")
+      .clear({ force: true })
+      .type(linkedin, { force: true });
+    cy.intercept({
+      method: "POST",
+      path: "/api/signatures/render",
+    }).as("signatureIsRendered");
+    cy.get("input[aid='signature_editor.input.1:appdata:target']")
+      .clear({ force: true })
+      .type(youtube, {
+        force: true,
+      });
+    cy.wait("@signatureIsRendered", { timeout: 10000 });
+    cy.get("input[aid='signature_editor.input.2:appdata:target']")
+      .clear({ force: true })
+      .type(instagram, { force: true });
+    cy.wait("@signatureIsRendered", { timeout: 10000 });
+    cy.get("input[aid='signature_editor.input.3:appdata:target']")
+      .clear({ force: true })
+      .type(twitter, {
+        force: true,
+      });
+    cy.wait("@signatureIsRendered", { timeout: 10000 });
+    cy.get("input[aid='signature_editor.input.4:appdata:target']")
+      .clear({ force: true })
+      .type(facebook, {
+        force: true,
+      });
+    cy.wait("@signatureIsRendered", { timeout: 10000 });
+    cy.get(".listControl").click();
+    cy.get("input[aid='signature_editor.input.5:appdata:target']")
+      .clear({ force: true })
+      .type(tiktok, {
+        force: true,
+      });
+    cy.wait("@signatureIsRendered", { timeout: 10000 });
+    cy.get("img[alt='App Social Buttons Image']", { timeout: 5000 });
+
+    cy.get(".btn__confirm").find("button").should("not.be.disabled");
+    cy.get(".btn__confirm").find("button").click({ force: true });
+    cy.wait("@signatureIsRendered", { timeout: 10000 });
+  }
+);
